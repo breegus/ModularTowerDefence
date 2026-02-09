3115ced2203e21f4164c0c570fad9ac7f85212b3
Anth <89168209+breegus@users.noreply.github.com>
2026-02-09 15:05:24 +0000

    Very basic health system

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
 
 namespace Enemies
 {
     public class Enemy : MonoBehaviour
     {
-        public float health = 100f;
+        public float health;
+
+        public Enemy(float health)
+        {
+            this.health = health;
+        }
         
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+            health = Math.Clamp(health, 0, 100);
+            
+            Debug.Log($"Ouch! I have {health} health left now.");
+        }
     }
 }
