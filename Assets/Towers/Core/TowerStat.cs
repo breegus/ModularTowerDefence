bdef93dc0acda891fa3b19bb1a8737b4794580d3
Anth <89168209+breegus@users.noreply.github.com>
2026-02-09 14:36:07 +0000

    Implemented statistics for modifiers

---
+++ Assets/Towers/Core/TowerStat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Towers.Core
+{
+    public class TowerStat
+    {
+        private float _baseValue;
+        private List<Func<float, float>> _modifiers = new();
+
+        public TowerStat(float baseValue)
+        {
+            _baseValue = baseValue;
+        }
+
+        public void AddModifier(Func<float, float> modifier)
+        {
+            _modifiers.Add(modifier);
+        }
+
+        public void RemoveModifier(Func<float, float> modifier)
+        {
+            _modifiers.Remove(modifier);
+        }
+
+        public float Value {
+            get
+            {
+                // Apply all current modifiers to the base value and return
+                return _modifiers.Aggregate(_baseValue, (current, modifier) => modifier(current));
+            }
+        }
+    }
+}
