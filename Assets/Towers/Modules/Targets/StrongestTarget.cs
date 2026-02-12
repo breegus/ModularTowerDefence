f7aecb15437702aa6882a5166d4302df8899d47a
Anthony Guy <89168209+breegus@users.noreply.github.com>
2026-02-12 13:50:03 +0000

    Added strongest target module

---
+++ Assets/Towers/Modules/Targets/StrongestTarget.cs
@@ -0,0 +1,21 @@
+﻿using Towers.Modules.Core;
+using UnityEngine;
+
+namespace Towers.Modules.Targets
+{
+    [CreateAssetMenu(menuName = "Towers/Modules/Targets/StrongestTarget")]
+    public class StrongestTarget : TargetingModule
+    {
+        protected override void UpdateTarget()
+        {
+            var didHaveTarget = Context.CurrentTarget;
+            Context.CurrentTarget = Context.Enemies.GetStrongest();
+            
+            if (Context.CurrentTarget)  // Target found / updated
+                Context.Events.TargetFound(Context.CurrentTarget);
+
+            if (didHaveTarget && !Context.CurrentTarget)  // Target lost
+                Context.Events.TargetLost();
+        }
+    }
+}
